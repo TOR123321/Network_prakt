93cf6db0dc27dab5a10d3255e49c9562a53084ff
TOR123321 <62548730+TOR123321@users.noreply.github.com>
2020-11-30 00:16:21 +0300

    Update main.cpp

--- daytime/main.cpp
+++ daytime/main.cpp
@@ -6,7 +6,7 @@
 #include <netinet/in.h>// sock_addr_in and allthing function  for network
 #include <arpa/inet.h>
 #include <sstream>
-#include <string>
+#include <string> 
 using namespace std;
 void errors(const char * why, const int exitCode = 1)
 {
@@ -65,40 +65,11 @@ int main(int argc, char **argv)
     buf[rc]='\0';
     cout<<"answer: "<<buf<<endl;
     // принятие ответа
-    stringstream response; // сюда будет записываться ответ клиенту
-     stringstream response_body; // тело ответа
      rc = recv(mySocket, buf, 256, 0);
      if (rc == -1) {
          close(mySocket);
          errors("Error recevie answer", 15);
-     } else {
-         // Мы знаем фактический размер полученных данных, поэтому ставим метку конца строки
-         // В буфере запроса.
-         buf[rc] = '\0';
-
-         // Данные успешно получены
-         // формировка тело ответа (HTML)
-         response_body << "<title>Test C++ HTTP Server</title>\n"
-                       << "<h1>Test page</h1>\n"
-                       << "<p>This is body of the test page...</p>\n"
-                       << "<h2>Request headers</h2>\n"
-                       << "<pre>" << buf << "</pre>\n"
-                       << "<em><small>Test C++ Http Server</small></em>\n";
-         // Формировка ответа вместе с заголовками
-         response << "HTTP/1.1 200 OK\r\n"
-                  << "Version: HTTP/1.1\r\n"
-                  << "Content-Type: text/html; charset=utf-8\r\n"
-                  << "Content-Length: " << response_body.str().length()
-                  << "\r\n\r\n"
-                  << response_body.str();
-         // Отправка ответа клиенту с помощью функции send
-         rc = send(mySocket, response.str().c_str(),response.str().length(), 0);
-         if (rc == -1) {
-             close(mySocket);
-             errors("Error send answer", 16);
-         }
-     }
-    
+     } 
     // закрытие сокета
     close(mySocket);
     // освобождение памяти
